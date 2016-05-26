51e2f81b7d8ba215cab1c23d518c0c19fe9a10ad
dltx <dltx@cy-pc>
2016-05-26 17:25:35 +0800

    123

---
+++ src/com/cy/util/ImageUtil.java
@@ -0,0 +1,39 @@
+package com.cy.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+public class ImageUtil {
+
+	public Drawable getBitFromInner(Context context,int resId)
+	{
+		BitmapFactory.Options opt = new BitmapFactory.Options();
+
+		opt.inPreferredConfig = Bitmap.Config.RGB_565;
+
+		opt.inPurgeable = true;
+
+		opt.inInputShareable = true;
+
+		//获取资源图片
+
+		InputStream is = context.getResources().openRawResource(resId);
+
+		Bitmap bitmap = BitmapFactory.decodeStream(is,null, opt);
+
+		try {
+			is.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return new BitmapDrawable(context.getResources(),bitmap);
+	}
+}
