e42104d8137d7766b97904a1650f67a09d76647f
dltx <dltx@cy-pc>
2016-05-26 17:24:13 +0800

    20160526

--- src/com/cy/changyoutianxia/MainActivity.java
+++ src/com/cy/changyoutianxia/MainActivity.java
@@ -1,14 +1,21 @@
 package com.cy.changyoutianxia;
 
+import com.cy.util.ImageUtil;
+
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
-
-	@Override
+	private LinearLayout mainLayout;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ImageUtil imUtil = new ImageUtil();
+		
+		mainLayout = (LinearLayout)findViewById(R.id.mainbg); 
+		mainLayout.setBackgroundDrawable(imUtil.getBitFromInner(MainActivity.this, R.drawable.bg));
 	}
 
 
