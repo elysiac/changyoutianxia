a62afcf529e2a416def21921a0b913dc5dcc0bb3
elyciac <cygreen5432@163.com>
2016-06-06 14:34:38 +0800

    123

--- src/com/cy/guide/GuideMainActivity.java
+++ src/com/cy/guide/GuideMainActivity.java
@@ -2,20 +2,24 @@ package com.cy.guide;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.GroundOverlayOptions;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.BaiduMap.OnMarkerDragListener;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.model.LatLngBounds;
 import com.cy.changyoutianxia.GroupActivity;
 import com.cy.changyoutianxia.R;
-import com.cy.util.ImageUtil;
 
 public class GuideMainActivity extends Activity {
 	/**
@@ -23,15 +27,22 @@ public class GuideMainActivity extends Activity {
 	 */
 	private MapView mMapView;
 	private BaiduMap mBaiduMap;
-
+	private Marker mMarkerA;
+	double[] city1=new double[2];
+	BitmapDescriptor bdGround = BitmapDescriptorFactory
+			.fromResource(R.drawable.ground_overlay);
+	BitmapDescriptor bdA = BitmapDescriptorFactory
+			.fromResource(R.drawable.icon_gcoding);
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_guide);
 		GroupActivity.activityList.add(GuideMainActivity.this);
 		mMapView = (MapView) findViewById(R.id.bmapView);
 		mBaiduMap = mMapView.getMap();
+		city1[0] = 40.611930;
+		city1[1] = 109.842569; //baotou
+		LatLng cenpt = new LatLng(city1[0], city1[1]); 
 		
-		LatLng cenpt = new LatLng(40.611930, 109.842569); 
         //定义地图状态
         MapStatus mMapStatus = new MapStatus.Builder()
         .target(cenpt)
@@ -41,10 +52,71 @@ public class GuideMainActivity extends Activity {
         MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
         //改变地图状态
         mBaiduMap.setMapStatus(mMapStatusUpdate);
+        initOverlay();
 	}
 
+	public void initOverlay() {
+		// add marker overlay
+		double[] adr = city1;
+		double[] adrSw = {adr[0]-0.002022,adr[1]+0.004395};
+		double[] adrNe = {adr[0]+0.00136,adr[1]-0.002342};
+//		LatLng llA = new LatLng(40.611930, 109.842569);
+		LatLng llA = new LatLng(adr[0], adr[1]);
+		OverlayOptions ooA = new MarkerOptions().position(llA).icon(bdA)
+				.zIndex(9).draggable(true);
+		mMarkerA = (Marker) (mBaiduMap.addOverlay(ooA));
+		// add ground overlay
+//		LatLng southwest = new LatLng(40.609908, 109.846964);
+//		LatLng northeast = new LatLng(40.61329, 109.840227);
+		LatLng southwest = new LatLng(adrSw[0], adrSw[1]);
+		LatLng northeast = new LatLng(adrNe[0], adrNe[1]);
+		LatLngBounds bounds = new LatLngBounds.Builder().include(northeast)
+				.include(southwest).build();
+
+		OverlayOptions ooGround = new GroundOverlayOptions()
+				.positionFromBounds(bounds).image(bdGround).transparency(0.8f);
+		mBaiduMap.addOverlay(ooGround);
+
+		MapStatusUpdate u = MapStatusUpdateFactory
+				.newLatLng(bounds.getCenter());
+		mBaiduMap.setMapStatus(u);
+
+		mBaiduMap.setOnMarkerDragListener(new OnMarkerDragListener() {
+			public void onMarkerDrag(Marker marker) {
+			}
+
+			public void onMarkerDragEnd(Marker marker) {
+				Toast.makeText(
+						GuideMainActivity.this,
+						"拖拽结束，新位置：" + marker.getPosition().latitude + ", "
+								+ marker.getPosition().longitude,
+						Toast.LENGTH_LONG).show();
+			}
+
+			public void onMarkerDragStart(Marker marker) {
+			}
+		});
+	}
 	
-	
+	protected void onPause() {
+		super.onPause();
+		// activity 暂停时同时暂停地图控件
+		mMapView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// activity 恢复时同时恢复地图控件
+		mMapView.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// activity 销毁时同时销毁地图控件
+		mMapView.onDestroy();
+	}
 	
 
 }
--- src/com/cy/util/MyApplication.java
+++ src/com/cy/util/MyApplication.java
@@ -2,7 +2,7 @@ package com.cy.util;
 
 import com.baidu.mapapi.SDKInitializer;
 import android.app.Application;
-//1 包头，2 鄂尔多斯
+
 public class MyApplication extends Application{  
 
 	@Override  
